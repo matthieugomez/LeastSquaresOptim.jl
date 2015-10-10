237611feca3b97c071df800305a1b5c05c3925a8
Matthieu Gomez <mattg@princeton.edu>
2015-10-09 20:40:17 -0400

    split

---
+++ src/solver/factorization_dense_cholesky.jl
@@ -0,0 +1,58 @@
+##############################################################################
+## 
+## solve J'J \ J'y by Cholesky (used in Dogleg)
+##
+##############################################################################
+
+type DenseCholeskySolver{Tc} <: AbstractSolver
+    chol::Tc
+end
+
+function allocate(nls::DenseLeastSquaresProblem,
+    ::Type{Val{:dogleg}}, ::Type{Val{:factorization_cholesky}})
+    return DenseCholeskySolver(Array(eltype(nls.J), length(nls.x), length(nls.x)))
+end
+
+function solve!(x, nls::DenseLeastSquaresProblem, solve::DenseCholeskySolver)
+    y, J = nls.y, nls.J
+    chol = solve.chol
+    
+    Ac_mul_B!(chol, J,  J)
+    Ac_mul_B!(x, J,  y)
+    A_ldiv_B!(cholfact!(chol, :U, Val{true}), x)
+    return 1
+end
+
+##############################################################################
+## 
+## solve (J'J + λ dtd) \ J'y by Cholesky (used in LevenbergMarquardt)
+##
+##############################################################################
+
+type DenseCholeskyDampenedSolver{Tc} <: AbstractSolver
+    chol::Tc
+end
+
+function allocate(nls:: DenseLeastSquaresProblem,
+    ::Type{Val{:levenberg_marquardt}}, ::Type{Val{:factorization_cholesky}})
+    return DenseCholeskyDampenedSolver(Array(eltype(nls.J), length(nls.x), length(nls.x)))
+end
+
+function solve!(x, dtd, λ, nls::DenseLeastSquaresProblem, solve::DenseCholeskyDampenedSolver)
+    y, J = nls.y, nls.J
+    chol = solve.chol
+    
+    # update chol as J'J + λdtd
+    Ac_mul_B!(chol, J, J)
+    clamp!(dtd, MIN_DIAGONAL, Inf)
+    scale!(dtd, λ)
+    @inbounds @simd for i in 1:size(chol, 1)
+        chol[i, i] += dtd[i]
+    end
+
+    # solve
+    Ac_mul_B!(x, J, y)
+    A_ldiv_B!(cholfact!(chol), x)
+    return 1
+end
+
