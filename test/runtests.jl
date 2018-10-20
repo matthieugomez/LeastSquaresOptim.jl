3b06bf43ab077e90e61b47914f12bbae5a13fc59
matthieugomez <gomez.matthieu@gmail.com>
2018-10-20 16:34:40 -0400

    add

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,9 @@
-using LeastSquaresOptim
+using LeastSquaresOptim, LinearAlgebra
 
 tests = ["nonlinearsolvers.jl", "nonlinearleastsquares.jl", "nonlinearfitting.jl"]
 
 println("Running tests:")
 
-# to pass dense lsmr
-import LinearAlgebra: mul!
-function mul!(C::Vector{Float64}, A::Matrix{Float64}, B::Vector{Float64}, α::Number, β::Number)
-    gemm!('N', 'N', convert(Float64, α), A, B, convert(Float64, β), C)
-end
-
-function mul!(C::Vector{Float64}, A::Adjoint{Float64, Matrix{Float64}}, B::Vector{Float64}, α::Number, β::Number)
-    gemm!('C', 'N', convert(Float64, α), A', B, convert(Float64, β), C)
-end
 
 for test in tests
 	try
